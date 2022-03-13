4514fd37ad0057e7f0af2df9013be2ee4f2f92e1
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-03-13 17:54:43 -0400

    fixing a () error in the type defs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,33 +1,33 @@
 // import the gql tagged template function
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 // create our typeDefs
 const typeDefs = gql`
-    type User {
-        _id: ID
-        username: String
-        email: String
-        gameCount: Int
-        savedGames: [Game]
-    }
-    
-    type Game {
-        _id: ID
-        gameId: String
-    }
-    type Query {
-        me: User
-    }
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveGame(): User
-        removeGame(): User
-    }
-    type Auth {
-        token: ID!
-        user: User
-    }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    gameCount: Int
+    savedGames: [Game]
+  }
+
+  type Game {
+    _id: ID
+    gameId: String
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveGame: User
+    removeGame: User
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
 // export the typeDefs
