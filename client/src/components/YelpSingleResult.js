916b807afeb8881ce75b7a49f04257f7e0cc91e1
Fayaz <flalani100@gmail.com>
2022-03-13 21:34:40 -0400

    Added placeholder for business name and address

--- client/src/components/YelpSingleResult.js
+++ client/src/components/YelpSingleResult.js
@@ -5,9 +5,19 @@ import styles from '../singleresult.module.css'
 const YelpSingleResult = () => {
     return ( 
         <div className = {styles.searchresult}> 
-            <img src ='https://via.placeholder.com/150'/>
-            <p>General Info</p>
-            <p>AddressData</p>
+            <img className = {styles.searchresultimg} src ='https://via.placeholder.com/105'/>
+            <div className = {styles.businessdetails}>
+                <h4 className = {styles.businessname}>Business Name</h4>
+                <p>Rating</p>
+                <p>$$  <span className = "tag">Sports Bar</span> <span className = "tag">Food</span></p>
+
+            </div>
+            <div>
+                <p>Number and Street Name</p>
+                <p>City Name</p>
+
+            </div>
+
         </div>
         
     )
