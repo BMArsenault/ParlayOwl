61de3e4141b4efc5ffe9024c4c9c5b3322bcd3ba
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-03-15 16:40:46 -0400

    removed front end mutations save game and remove game. formatted files with prettier

--- client/src/components/YelpResults.js
+++ client/src/components/YelpResults.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import YelpSingleResult from './YelpSingleResult';
+import React from "react";
+import YelpSingleResult from "./YelpSingleResult";
 
 const YelpResults = () => {
-
-    return (
-        <div> 
-            <YelpSingleResult/>
-            <YelpSingleResult/>
-            <YelpSingleResult/>
-        </div>
-
-    )
-
-}
+  return (
+    <div>
+      <YelpSingleResult />
+      <YelpSingleResult />
+      <YelpSingleResult />
+    </div>
+  );
+};
 
 export default YelpResults;
